d178892a1e56d16eb919f9a37d0cfc72bfc9fa38
Justin Cranford <craju03@ca.com>
2016-04-15 13:03:55 -0400

    Added draft rest project.

---
+++ rest/src/main/java/org/justin/rest/App.java
@@ -0,0 +1,13 @@
+package org.justin.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends javax.ws.rs.core.Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
